40e558c2adab25d10acef03f9b3a96765bf61cc2
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-02-04 17:40:45 +0100

    - add support for cors

--- src/main/java/com/nprogramming/weather/WeatherSearchApplication.java
+++ src/main/java/com/nprogramming/weather/WeatherSearchApplication.java
@@ -5,8 +5,12 @@ import com.nprogramming.weather.data.WeatherRepository;
 import io.dropwizard.Application;
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.ws.rs.client.Client;
+import java.util.EnumSet;
 
 public class WeatherSearchApplication extends Application<WeatherSearchConfiguration> {
 
@@ -18,6 +22,8 @@ public class WeatherSearchApplication extends Application<WeatherSearchConfigura
     public void run(
             WeatherSearchConfiguration config,
             Environment environment) throws Exception {
+
+        configureCors(environment);
 
         final Client client = new JerseyClientBuilder(environment)
                 .using(config.getJerseyClientConfiguration())
@@ -39,6 +45,17 @@ public class WeatherSearchApplication extends Application<WeatherSearchConfigura
         environment.healthChecks().register("city", healthCheck);
         environment.jersey().register(resource);
     }
+
+    private void configureCors(Environment environment) {
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS,PATCH");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
+    }
+
 
     @Override
     public String getName() {
